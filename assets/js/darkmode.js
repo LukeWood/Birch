9d5f5f0fb99f0a21947ccb054c542bab90f636bc
Luke Wood <lukewoodcs@gmail.com>
2019-11-09 16:32:38 -0800

    nigth mode button

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,10 +1,30 @@
-if (localStorage.getItem("preferredTheme") === null) {
-  var theme = (window.matchMedia && window.matchMedia("(prefers-color-scheme: light)").matches) ? "light" : "dark"
-  localStorage.setItem("preferredTheme", theme);
-}
+window.addEventListener("load", function() {
 
-function getTheme() {
-  return window.localStorage.getItem("preferredTheme") || "dark";
-}
+  if (localStorage.getItem("preferredTheme") === null) {
+    var theme = (window.matchMedia && window.matchMedia("(prefers-color-scheme: light)").matches) ? "light" : "dark"
+    localStorage.setItem("preferredTheme", theme);
+  }
 
-document.documentElement.setAttribute("data-theme", getTheme());
+  function getTheme() {
+    return window.localStorage.getItem("preferredTheme") || "dark";
+  }
+
+  document.documentElement.setAttribute("data-theme", getTheme());
+
+
+  document.getElementById("night-toggle").addEventListener("click", function() {
+    var current = document.documentElement.getAttribute("data-theme");
+    var next = null
+    if (current == "dark") {
+      next = "light";
+    } else {
+      next = "dark";
+    }
+    document.documentElement.setAttribute("data-theme", next);
+
+    if(window.localStorage) {
+      window.localStorage.setItem("preferredTheme", next);
+    }
+  });
+
+})
